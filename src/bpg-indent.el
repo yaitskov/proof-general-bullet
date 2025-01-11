fe12c6eabd301402ef6fc5a9ed5937bd53e1e0b2
Daniil Iaitskov <dyaitskov@gmail.com>
2025-01-10 20:13:16 -0900

    master bpg-indent-right and bpg-indent-left

--- src/bpg-indent.el
+++ src/bpg-indent.el
@@ -10,8 +10,7 @@
 
 (defun gen-bullet-indent-alist (bullet-levels shift-fn)
   (hash-table-of-alist
-   (seq-mapn 'cons bullet-levels (funcall shift-fn bullet-levels))
-   (make-hash-table :test 'equal)))
+   (seq-mapn 'cons bullet-levels (funcall shift-fn bullet-levels))))
 
 (defun gen-bullet-indent-right-alist (bullet-levels)
   "hash table mapping:  - => +; * => --  ; ..."
@@ -22,6 +21,60 @@
   (gen-bullet-indent-alist
    bullet-levels
    (apply-partially 'cons "")))
+
+(defcustom bpg-bullet-abc "-+*" "bullet characters in the order")
+
+(defvar bpg-bullet-levels (apply 'append
+                                 (gen-rewrite-rules
+                                  (mapcar 'char-to-string
+                                          (string-to-list bpg-bullet-abc))
+                                  2)))
+(defvar bpg-indent-left-map (gen-bullet-indent-left-alist bpg-bullet-levels))
+(defvar bpg-indent-right-map (gen-bullet-indent-right-alist bpg-bullet-levels))
+
+(defun bpg-indent-region (bullet-map)
+  (with-undo-amalgamate
+    (save-excursion
+      (letrec ((start-line (line-number-at-pos (region-beginning)))
+               (lines-in-region
+                (- (1+ (line-number-at-pos (region-end)))
+                   start-line)))
+        (goto-char (region-beginning))
+        ;; (message "start line %d; lines in region: %d;" start-line lines-in-region)
+        (line-beginning-position)
+        (while (> lines-in-region 0)
+          (skip-chars-forward " ")
+          (let ((bullet-start (point))
+                (bullet-len (skip-chars-forward bpg-bullet-abc)))
+            ;; (message "bullet len  %d; point: %d" bullet-len (point))
+            (when (> bullet-len 0)
+              (letrec ((bullet-end (point))
+                       (bullet (buffer-substring bullet-start bullet-end)))
+                (when (> (skip-chars-forward " ") 0)
+                  ;; (message "space after bullet")
+                  (let ((new-bullet (gethash bullet bullet-map)))
+                    ;; (message "new bullet: %s" new-bullet)
+                    (when new-bullet
+                      (replace-string-in-region bullet new-bullet bullet-start bullet-end))))))
+            )
+          (setq lines-in-region (- lines-in-region 1))
+          (forward-line)
+          )
+        )
+      )
+    )
+  )
+
+
+(defun bpg-indent-left ()
+  "walk through the region by line match line prefix"
+  (interactive)
+  (bpg-indent-region bpg-indent-left-map))
+
+(defun bpg-indent-right ()
+  "walk through the region by line match line prefix"
+  (interactive)
+  (bpg-indent-region bpg-indent-right-map))
 
 (provide 'bpg-indent)
 ;;; bgp-indent.el ends here
--- test/indent.el
+++ test/indent.el
@@ -17,3 +17,18 @@
   (should (equal (gethash "bb"
                           (gen-bullet-indent-right-alist  '("a" "b" "aa" "bb" "aaa" "bbb")))
                  "aaa")))
+
+(defun test-bpg-indent-erts (ert-file indent-fun)
+  (ert-test-erts-file ert-file
+                      (lambda ()
+                        (goto-char (point-min))
+                        (push-mark)
+                        (goto-char (point-max))
+                        (funcall indent-fun)
+                        )))
+
+(ert-deftest bpg-indent-left-erts ()
+  (test-bpg-indent-erts "indent-left-whole-file.erts" 'bpg-indent-left))
+
+(ert-deftest bpg-indent-right-erts ()
+  (test-bpg-indent-erts "indent-right-whole-file.erts" 'bpg-indent-right))
