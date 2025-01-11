b9540d6f2b359adba26bbac2bac603d3b4427824
Daniil Iaitskov <dyaitskov@gmail.com>
2025-01-10 18:06:42 -0900

    master remove dash package from dependencies and use seq-mapn instead of -zip-with

--- src/bpg-indent.el
+++ src/bpg-indent.el
@@ -1,17 +1,16 @@
 ;;; bgp-indent.el --- rewrite bullets to increase nesting level                -*- lexical-binding: t; -*-
-(require 'dash)
 (require 'bpg-collection)
 
 (cl-defun gen-rewrite-rules (abc n &optional (origin ()))
   " generate sequence: '(- + -- ++ ...) from alphabet '(- +)"
   (if (> n 0)
       (gen-rewrite-rules abc (1- n)
-                         (cons abc (mapcar (lambda (l) (-zip-with 'concat abc l)) origin)))
+                         (cons abc (mapcar (lambda (l) (seq-mapn 'concat abc l)) origin)))
     origin))
 
 (defun gen-bullet-indent-alist (bullet-levels shift-fn)
   (hash-table-of-alist
-   (-zip-with 'cons bullet-levels (funcall shift-fn bullet-levels))
+   (seq-mapn 'cons bullet-levels (funcall shift-fn bullet-levels))
    (make-hash-table :test 'equal)))
 
 (defun gen-bullet-indent-right-alist (bullet-levels)
